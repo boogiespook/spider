bd92bdbf04df6fd1f486c3baaa31672d6b7ea9c1
ChrisJ <cj@chrisj.com>
2017-01-10 16:22:42 +0000

    Dynamic host identification added

--- index.php
+++ index.php
@@ -426,8 +426,11 @@ $areas = array(
 
 
 ## Database stuff
-$db = mysqli_connect('172.30.158.48','adminzvJZccK','5Et6HuymAm_j');
-#$db = mysqli_connect('localhost','adminzvJZccK','5Et6HuymAm_j');
+if ($_SERVER["HTTP_HOST"] == "localhost") {
+	$db = mysqli_connect('localhost','adminzvJZccK','5Et6HuymAm_j');
+} else {
+	$db = mysqli_connect('172.30.158.48','adminzvJZccK','5Et6HuymAm_j');
+}
 	if (!$db) {
 	die("Unable to connect to database");
 	}
