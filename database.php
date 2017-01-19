35a35926c1b28c926877351aefb91a8cf0828cdd
ChrisJ <cj@chrisj.com>
2017-01-19 11:12:37 +0000

    hostname updated

--- database.php
+++ database.php
@@ -5,7 +5,8 @@ global $db;
 if ($_SERVER["HTTP_HOST"] == "localhost") {
 	$db = mysqli_connect('localhost','adminzvJZccK','5Et6HuymAm_j');
 } else {
-	$db = mysqli_connect('mysql-innovate.44fs.preview.openshiftapps.com','adminzvJZccK','5Et6HuymAm_j');
+	#$db = mysqli_connect('mysql-innovate.44fs.preview.openshiftapps.com','adminzvJZccK','5Et6HuymAm_j');
+	$db = mysqli_connect('172.30.158.48','adminzvJZccK','5Et6HuymAm_j');
 }
 	if (!$db) {
 	die("Unable to connect to database");
