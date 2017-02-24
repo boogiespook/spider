b0f1d9e2e07872acff8b4e68d29eb7b0e3bccfd7
ChrisJ <cj@chrisj.com>
2017-02-24 10:12:56 +0000

    Final update

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,11 @@
 <?php
 function connectDB() {
 ## Database stuff
+$db_host = getenv('OPENSHIFT_MYSQL_DB_HOST'); 
+$db_user = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$db_pass = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 global $db;
-$db = mysql_connect('127.7.155.130','adminzvJZccK','5Et6HuymAm_j');
+$db = mysql_connect($db_host,$db_user,$db_pass);
 	if (!$db) {
 	die("Unable to connect to database");
 	}
