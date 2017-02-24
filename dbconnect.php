52b94093fc9dd4cd3aec3896aa9533098845568d
ChrisJ <cj@chrisj.com>
2017-02-24 09:47:09 +0000

    Another bloody update

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
 function connectDB() {
 ## Database stuff
 global $db;
-$db = mysql_connect('spider-cjenkins.rhcloud.com','adminzvJZccK','5Et6HuymAm_j');
+$db = mysql_connect('127.7.155.130','adminzvJZccK','5Et6HuymAm_j');
 	if (!$db) {
 	die("Unable to connect to database");
 	}
